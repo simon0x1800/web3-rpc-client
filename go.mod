63fa90da17133fc9159d8235277a8988a059d909
Joseph Bao <jobaodev@gmail.com>
2024-12-01 04:19:19 -0800

    Init Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/simon0x1800/web3-rpc-client
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,7 @@
+package web3_rpc_client
+
+import "fmt"
+
+func main() {
+	fmt.Println("Web3 RPC Client Version 0.0.1")
+}
